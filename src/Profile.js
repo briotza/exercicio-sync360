e890f274c0f9707d1bdb18182e227cb5dc37ac12
briotza <cianomeliunas@gmail.com>
2024-05-07 21:37:13 -0300

    feat: divisões da coluna de perfil

--- src/Profile.js
+++ src/Profile.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function Profile({ profile }) {
     return (
-        
+        <div>
+            <div className='col-md-4 bg-secondary mb-3'>
+                <span>Coluna 1</span>
+            </div>
+            <div className='col-md-6 bg-success mb-3'>
+                <span>Coluna 1</span>
+            </div>
+            <div className='col-md-2 bg-warning mb-3'>
+                <span>Coluna 1</span>
+            </div>
+        </div>
     )
 }
 
