4fca68e82a87de0016fb511c4d7141894229535c
briotza <cianomeliunas@gmail.com>
2024-05-08 01:17:47 -0300

    feat: adicionei verificação para entradas e branco

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -22,6 +22,12 @@ function EditProfile({ edit, cancelEdit }) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        for (const key in formData) {
+            if (formData.hasOwnProperty(key) && typeof formData[key] === 'string' && formData[key].trim() === '') {
+                alert(`O campo ${key} é obrigatório`);
+                return;
+            }
+        }
         edit(formData);
         cancelEdit();
     };
@@ -36,27 +42,27 @@ function EditProfile({ edit, cancelEdit }) {
                     <form onSubmit={handleSubmit}>
                         <div className='mb-3'>
                             <label className='form-label'>Nome:</label>
-                            <input type='text' className='form-control' id='nome' name='nome' value={FormData.nome} onChange={handleChange} />
+                            <input type='text' className='form-control' id='nome' name='nome' value={FormData.nome} onChange={handleChange} required />
                         </div>
                         <div className='mb-3'>
                             <label className='form-label'>Idade:</label>
-                            <input type='number' className='form-control' id='idade' name='idade' value={FormData.idade} onChange={handleChange} />
+                            <input type='number' className='form-control' id='idade' name='idade' value={FormData.idade} onChange={handleChange} required />
                         </div>
                         <div className='mb-3'>
                             <label className='form-label'>Rua:</label>
-                            <input type='text' className='form-control' id='rua' name='rua' value={FormData.rua} onChange={handleChange} />
+                            <input type='text' className='form-control' id='rua' name='rua' value={FormData.rua} onChange={handleChange} required />
                         </div>
                         <div className='mb-3'>
                             <label className='form-label'>Bairro:</label>
-                            <input type='text' className='form-control' id='bairro' name='bairro' value={FormData.bairro} onChange={handleChange} />
+                            <input type='text' className='form-control' id='bairro' name='bairro' value={FormData.bairro} onChange={handleChange} required />
                         </div>
                         <div className='mb-3'>
                             <label className='form-label'>Estado:</label>
-                            <input type='text' className='form-control' id='estado' name='estado' value={FormData.estado} onChange={handleChange} />
+                            <input type='text' className='form-control' id='estado' name='estado' value={FormData.estado} onChange={handleChange} required />
                         </div>
                         <div className='mb-4'>
                             <label className='form-label'>Biografia:</label>
-                            <textarea className='form-control' id='bio' name='bio' value={FormData.bio} onChange={handleChange} />
+                            <textarea className='form-control' maxLength={400} id='bio' name='bio' value={FormData.bio} onChange={handleChange} required  />
                         </div>
                         <button className='btn btn-primary me-2'>SALVAR</button>
                         <button className='btn btn-secondary' onClick={cancelEdit}>CANCELAR</button>
