4165ebc7fc4a0bbae012b03e4f520f1a2d709174
briotza <cianomeliunas@gmail.com>
2024-05-07 22:21:53 -0300

    feat: adicionei imagem de perfil

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import profileImage from './img/profile.jpg';
 
 function Profile({ profile }) {
     return (
-        <div>
-            <div className='col-md-4 bg-secondary mt-3'>
-                <span>Coluna 1</span>
+        <div className="d-flex flex-column align-items-center">
+            <div className='col-md-4 bg-secondary mt-3 text-center'>
+                <img src={profileImage} alt='user-image' style={{borderRadius: '100%', width: '60%'}}/>
             </div>
             <div className='col-md-6 bg-success mt-3'>
                 <span>Coluna 1</span>
