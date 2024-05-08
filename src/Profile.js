610156badbd1ccf98463c3000ec9d9b525be12a4
briotza <cianomeliunas@gmail.com>
2024-05-07 21:48:14 -0300

    style: defini margin top

--- src/Profile.js
+++ src/Profile.js
@@ -3,13 +3,13 @@ import React from 'react';
 function Profile({ profile }) {
     return (
         <div>
-            <div className='col-md-4 bg-secondary mb-3'>
+            <div className='col-md-4 bg-secondary mt-3'>
                 <span>Coluna 1</span>
             </div>
-            <div className='col-md-6 bg-success mb-3'>
+            <div className='col-md-6 bg-success mt-3'>
                 <span>Coluna 1</span>
             </div>
-            <div className='col-md-2 bg-warning mb-3'>
+            <div className='col-md-2 bg-warning mt-3'>
                 <span>Coluna 1</span>
             </div>
         </div>
