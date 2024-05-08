ac831555c7b6205a00ec96ebe3524e52c8cd17da
briotza <cianomeliunas@gmail.com>
2024-05-07 23:30:08 -0300

    feat: área de endereço

--- src/Profile.js
+++ src/Profile.js
@@ -6,7 +6,7 @@ function Profile({ profile }) {
     return (
         <div className="d-flex flex-column align-items-center">
             <div className='col-md-4 text-center top-profile'>
-                <img src={profileImage} alt='user-image' className='top-profile-img'/>
+                <img src={profileImage} alt='user-image' className='top-profile-img' />
             </div>
             <div className='col-md-6 user-info'>
                 <div className='card-body text-center'>
@@ -15,8 +15,20 @@ function Profile({ profile }) {
                     <p className='bio'>João, 22 anos, é um estudante de cinema apaixonado pela arte de contar histórias através das lentes da câmera. Desde tenra idade, sua imaginação foi cativada pelas telas do cinema, e agora ele dedica seus dias na faculdade de cinema, transformando sua paixão em profissão. Com olhos atentos e mente criativa, ele está determinado a deixar sua marca na indústria cinematográfica, um quadro por vez.</p>
                 </div>
             </div>
-            <div className='col-md-2 bg-warning mt-3'>
-                <span>Coluna 1</span>
+            <div className='row mt-5 endereco'>
+                <div className='col-md-4'>
+                    <h5>Rua</h5>
+                    <p>Rua Alguma Coisa</p>
+                </div>
+                <div className='col-md-4'>
+                    <h5>Bairro</h5>
+                    <p>Bairro Alguma Coisa</p>
+                </div>
+                <div className='col-md-4'>
+                    <h5>Estado</h5>
+                    <p>Rio de Janeiro</p>
+                </div>
+
             </div>
         </div>
     )
