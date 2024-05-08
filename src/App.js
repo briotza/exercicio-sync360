53d5e82c931a933625da63d4dbebc2e9847721a1
briotza <cianomeliunas@gmail.com>
2024-05-07 21:20:18 -0300

    feat: divisões principais da página

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-   <p>teste</p>
+   <div className="container-fluid">
+    <div className="row">
+      {/*Menu Lateral de Navegação*/}
+      <div className="col-md-2 bg-primary">
+        <p>navegação</p>
+      </div>
+
+      {/*Perfil*/}
+      <div className="col-md-8">
+        <p>Perfil</p>
+      </div>
+
+      {/*Página Acessada*/}
+      <div className="col-md-2 bg-info">
+
+      </div>
+    </div>
+   </div>
   );
 }
 
