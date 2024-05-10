a04ffe4f1358bc928483ce9e7119d322b1d048fa
briotza <cianomeliunas@gmail.com>
2024-05-09 22:27:44 -0300

    feat: inseri mensagem de log para servidor rodando

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const userRoutes = require("./routes/users.js"); // Usando require em vez de import
+const userRoutes = require("./routes/users"); // Usando require em vez de import
 const cors = require('cors');
+const db = require('./db');
 
 const app = express();
 
 app.use(express.json());
+
 app.use(cors());
 
 app.use("/", userRoutes);
 
-app.listen(8800);
+const port = 8800; 
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
