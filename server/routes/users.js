3edd1886ba2d48fcd9936d6ada7ebbb0bcd574b5
briotza <cianomeliunas@gmail.com>
2024-05-09 22:42:02 -0300

    feat: rota para pegar user por id

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,11 @@ const express = require('express');
 const UserController = require('../controllers/user');
 const router = express.Router();
 
+// Rota para pegar todos os usuários
 router.get('/', UserController.getAllUsers);
+
+// Rota para pegar usuário por id
+router.get('/:id', UserController.getUserById);
 
 
 module.exports = router; 
